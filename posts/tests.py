f569c9f016b51065931dd8dd2ff0664fe36840b4
mahadley <mark.hadley1@hotmail.com>
2021-05-03 17:32:28 -0700

    third commit

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="Just a test")
+
+    def test-text_content(self):
+        post = Post.objects,get(id=1)
+        expected_obj_name =f"{post.text}"
+        self.assertEqual(expected_obj_name, "Just a test")
